47a5d3652c0804f3ce27ac48d1edaff0b3e17b49
Rupali <rupaligupta2707@gmail.com>
2021-11-15 17:02:57 +0530

    modified

--- app.py
+++ app.py
@@ -1,25 +1,39 @@
-from flask import Flask
-from flask_mail import Mail, Message
-import os
+from flask import Flask, render_template,request
 
-app = Flask(__name__)
+from flask_mail import Mail,Message
+
+app= Flask(__name__)
+
 
 mail_settings = {
     "MAIL_SERVER": 'smtp.gmail.com',
     "MAIL_PORT": 465,
     "MAIL_USE_TLS": False,
     "MAIL_USE_SSL": True,
-    "MAIL_USERNAME": 'email',  #enter email
-    "MAIL_PASSWORD": 'password' #enter password
+    "MAIL_USERNAME": 'grupali645@gmail.com',  #enter email
+    "MAIL_PASSWORD": '123456@@' #enter password
 }
-
 app.config.update(mail_settings)
-mail = Mail(app)
+mail= Mail(app)
 
-if __name__ == '__main__':
-    with app.app_context():
-        msg = Message(subject="Hello",
-                      sender=app.config.get("MAIL_USERNAME"),
-                      recipients=["recipient"], # replace with your email for testing
-                      body="This is a test email I sent with Gmail and Python!")
-        mail.send(msg)
+@app.route('/')
+def index():
+    return render_template("home.html")
+
+
+@app.route('/send_mail',methods=['GET','POST'])
+def send_mail():
+    if request.method=="POST":
+        email=request.form['email']
+        subject=request.form['subject']
+        msg=request.form['message']
+
+        message=Message(subject,sender="grupali645@gmail.com",recipients=[email])
+        message.body=msg
+
+        mail.send(message)
+        success="Mail sent"
+        return render_template("result.html",success=success)
+
+if __name__=="__main__":
+    app.run(debug=True)
