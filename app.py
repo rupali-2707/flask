ff7f04ac39236f6979c8766672f6c89f07d115dc
Rupali <rupaligupta2707@gmail.com>
2021-11-20 16:24:16 +0530

    modified

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template,request
 from flask import flash, redirect, request
 from flask_mail import Mail,Message
+import os
+import csv
+
 
 app= Flask(__name__)
+APP_ROOT=os.path.dirname(os.path.abspath(__file__))
 
 def stmp_setting(stmp_email,port,sender,password):
     # mail_settings = {
@@ -23,7 +27,6 @@ def stmp_setting(stmp_email,port,sender,password):
     }
     app.config.update(mail_settings)
 
-    
 
 @app.route('/')
 def index():
@@ -39,23 +42,69 @@ def send_mail():
         port=request.form['port']
         sender=request.form['sender_email']
         password=request.form['password']
+        
+        subject=request.form['subject']
+        msg=request.form['message']
 
         stmp_setting(stmp,port,sender,password)
         mail= Mail(app)
-        email=request.form['email']
-        subject=request.form['subject']
-        msg=request.form['message']
-        email=email.splitlines() #convert into list
-        print(email)
 
-        message=Message(subject,sender=sender,recipients=email)  #passing list of receipents
-        message.body=msg
 
-        if not (mail.send(message)):
-            flash("Mail sent Successful","success")
-            return redirect(request.url)
-        flash("Mail send Failed","danger")
-        return redirect(request.url)
+        if request.form["submit_button"]=='test':    #for testing
+            email=request.form['email']
+            email=email.splitlines() #convert into list
+            print(email)
+            message=Message(subject,sender=sender,recipients=email)  #passing list of receipents
+            message.body=msg
+
+            if not (mail.send(message)):
+                flash("Test Mail sent Successful","success")
+                return redirect(request.url)
+            flash("Test Mail send Failed","danger")
+        
+
+
+        if request.form["submit_button"]=='bulk sending':
+            target=os.path.join(APP_ROOT,'csv_file/')
+            print("target:\t",target)
+
+            if not os.path.isdir(target):
+                os.mkdir(target)
+            for file in request.files.getlist("file"):
+                print(file)
+                filename=file.filename
+                print(filename) #file name
+                des="/".join([target,filename])
+                print("des\t:",des)
+                file.save(des)
+            try:
+                with open(r'C:\Users\lenovo\Desktop\flask\csv_file\{}'.format(filename), newline='') as f:
+                        print("hey")
+                        reader = csv.reader(f)
+                        list1 = list(reader)
+
+                print(list1) 
+                flat_list = []
+                for sublist in list1:
+                        for item in sublist:
+                            flat_list.append(item)
+                print(flat_list)
+
+            # if request.form["submit_button"]=='bulk':
+                if not flat_list:
+                            flash("you did not load properly","danger")
+                else:
+                                message=Message(subject,sender=sender,recipients=flat_list)  #passing list of receipents
+                                message.body=msg
+
+                                if not (mail.send(message)):
+                                    flash("Mail sent Successful","success")
+                                    return redirect(request.url)
+                                flash("Mail send Failed","danger")
+                                return redirect(request.url)
+            except:
+                flash("you did not load properly","danger")
+                return redirect(request.url)
 
     return render_template("home.html")
 if __name__=="__main__":
